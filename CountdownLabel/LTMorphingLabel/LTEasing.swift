4b7546ec6e88c0e6833a3261aed42efb17f68934
Mohammed Eldabbagh <mdabbagh888@gmail.com>
2021-11-10 20:22:30 +0300

    Update LTEasing.swift
    fix

--- CountdownLabel/LTMorphingLabel/LTEasing.swift
+++ CountdownLabel/LTMorphingLabel/LTEasing.swift
@@ -18,7 +18,7 @@ public struct LTEasing {
     
     public static func easeOutQuint(_ t: Float, _ b: Float, _ c: Float, _ d: Float = 1.0) -> Float {
         return {
-            let x = pow($0, 4) * $0 * $0 * $0 * $0
+            let x = $0 * $0 * $0 * $0
             return c * (x + 1.0) + b
             }(t / d - 1.0)
     }
